a9b674120ed5201c5615f4015c50b5dd3e4d92b7
harsh24924 <hds24924@gmail.com>
2025-09-06 23:51:22 +0530

    added more tests

--- Backend/tests.py
+++ Backend/tests.py
@@ -5,16 +5,20 @@ from fastapi.testclient import TestClient
 
 client = TestClient(app)
 
-def test_success():
-    mock_resume = mock_resume = {
-            "summary": "Enthusiastic and creative recent Graphic Design graduate with a strong portfolio showcasing expertise in visual communication, branding, and digital media. Eager to apply design principles and software proficiency to deliver impactful visual solutions in a dynamic environment.",
-            "skills": "Proficient in Adobe Creative Suite (Photoshop, Illustrator, InDesign, After Effects, Premiere Pro), Figma, Sketch, Canva. Strong understanding of typography, color theory, layout design, branding, and web design principles. Experienced in digital illustration, motion graphics, and UI/UX basics.",
-            "education": "Bachelor of Fine Arts in Graphic Design from Rhode Island School of Design, Providence, RI (Graduated: May 2023).",
-            "projects": "Designed comprehensive brand identity for a local startup, including logo, style guide, and marketing collateral. Developed UI/UX mockups and interactive prototypes for a mobile application concept. Created a series of animated social media campaigns for a non-profit organization.",
-            "experience": "Graphic Design Intern at Creative Solutions Agency (Summer 2022), assisting with client projects, brand development, and digital content creation. Freelance Graphic Designer (2021-Present), collaborating with small businesses on website graphics, print materials, and social media assets.",
-            "certifications": "Adobe Certified Professional in Graphic Design & Illustration Using Adobe Illustrator."
-        }
+@pytest.fixture
+def mock_resume():
+    resume = {
+        "summary": "Enthusiastic and creative recent Graphic Design graduate with a strong portfolio showcasing expertise in visual communication, branding, and digital media. Eager to apply design principles and software proficiency to deliver impactful visual solutions in a dynamic environment.",
+        "skills": "Proficient in Adobe Creative Suite (Photoshop, Illustrator, InDesign, After Effects, Premiere Pro), Figma, Sketch, Canva. Strong understanding of typography, color theory, layout design, branding, and web design principles. Experienced in digital illustration, motion graphics, and UI/UX basics.",
+        "education": "Bachelor of Fine Arts in Graphic Design from Rhode Island School of Design, Providence, RI (Graduated: May 2023).",
+        "projects": "Designed comprehensive brand identity for a local startup, including logo, style guide, and marketing collateral. Developed UI/UX mockups and interactive prototypes for a mobile application concept. Created a series of animated social media campaigns for a non-profit organization.",
+        "experience": "Graphic Design Intern at Creative Solutions Agency (Summer 2022), assisting with client projects, brand development, and digital content creation. Freelance Graphic Designer (2021-Present), collaborating with small businesses on website graphics, print materials, and social media assets.",
+        "certifications": "Adobe Certified Professional in Graphic Design & Illustration Using Adobe Illustrator."
+    }
 
+    yield resume
+
+def test_success(mock_resume):
     response = client.post("/recommend/", json = mock_resume)
     internships = response.json()
 
@@ -29,3 +33,40 @@ def test_success():
             Internship(**internship)
         except Exception as e:
             pytest.fail(f"An item in the response list failed Pydantic validation: {e}")
+
+def test_missing_field(mock_resume):
+    del mock_resume["projects"]
+
+    response = client.post("/recommend/", json = mock_resume)
+    response_details = response.json()
+
+    assert response.status_code == 422
+    assert response_details["detail"][0]["type"] == "missing"
+    assert response_details["detail"][0]["loc"] == ["body", "projects"]
+
+def test_missing_json_payload():
+    response = client.post("/recommend/")
+    response_details = response.json()
+
+    assert response.status_code == 422
+    assert response_details["detail"][0]["type"] == "missing"
+    assert response_details["detail"][0]["loc"] == ["body"]
+
+def test_wrong_data_type(mock_resume):
+    mock_resume["skills"] = ["C++", "Java"]
+    response = client.post("/recommend/", json = mock_resume)
+    response_details = response.json()
+
+    assert response.status_code == 422
+    assert response_details["detail"][0]["loc"] == ["body", "skills"]
+    assert response_details["detail"][0]["msg"] == "Input should be a valid string"
+
+
+def test_empty_field(mock_resume):
+    mock_resume["skills"] = None
+    response = client.post("/recommend/", json = mock_resume)
+    response_details = response.json()
+
+    assert response.status_code == 422
+    assert response_details["detail"][0]["loc"] == ["body", "skills"]
+    assert response_details["detail"][0]["msg"] == "Input should be a valid string"
